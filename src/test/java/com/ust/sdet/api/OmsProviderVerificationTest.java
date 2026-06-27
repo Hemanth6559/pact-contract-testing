1c5d348b011afd64c93a3582450d8c3ebd4828fc
Hemanth Venkata Reddy Tetali(UST <299167@ust.com>
2026-06-27 14:34:35 +0530

    added .github

--- src/test/java/com/ust/sdet/api/OmsProviderVerificationTest.java
+++ src/test/java/com/ust/sdet/api/OmsProviderVerificationTest.java
@@ -38,6 +38,7 @@ class OmsProviderVerificationTest {
         context.verifyInteraction();
     }
 
+
     @State("order 1 exists")
     void orderExists() {
         System.out.println("Preparing Order 1");
